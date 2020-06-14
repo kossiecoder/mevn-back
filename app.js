2b7e2f0650d58eddae4050038077cc78c1e2cce2
Kossie Coder <kossie.coder@gmail.com>
2020-06-14 15:49:18 +1000

    04

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import bodyParser from 'body-parser';
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+const { body, validationResult } = require("express-validator");
+
+
 dotenv.config();
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/auth/signup', (req, res, next) => {
+app.post('/auth/signup', [
+  body('email')
+    .isEmail()
+    .withMessage('Please type valid email'),
+  body('password')
+    .trim()
+    .isLength({ min: 6 })
+    .withMessage('Password must be greater than 6 characters'),
+  body('name')
+    .trim()
+    .not().isEmpty()
+    .withMessage('Name field is required')
+], (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        const error = new Error('Validation failed');
+        error.statusCode = 422;
+        error.data = errors.array();
+        throw error;
+    }
     console.log(req.body);
     const email = req.body.email;
     const password = req.body.password;
@@ -15,6 +38,15 @@ app.post('/auth/signup', (req, res, next) => {
         message: 'success'
     })
 });
+
+app.use((error, req, res, next) => {
+    const status = error.statusCode || 500;
+    const message = error.message;
+    const data = error.data;
+    res.status(status).json({
+        message, data
+    })
+})
 
 mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0-sptrf.mongodb.net/taskmanager?retryWrites=true&w=majority`).then(() => {
     app.listen(3000, () => console.log('listening to port 3000'));
--- models/user.js
+++ models/user.js
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
@@ -16,4 +16,4 @@ const userSchema = new Schema({
 	},
 });
 
-export default mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', userSchema);
